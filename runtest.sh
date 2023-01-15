20a9354d2549e16aaa4d300e20a931303a08ec83
Cody McDonald <codylmcdonald@gmail.com>
2023-01-15 06:33:32 -0800

    I added readout indicating the test has started. Also added sleep for duration of test and a readout after test is finished.

--- runtest.sh
+++ runtest.sh
@@ -1,3 +1,9 @@
  #!/bin/bash
  #This script is used to test switch ports.
  ip="8.8.8.8"; count="25"; nohup ping -c ${count} ${ip} &>> /home/ansible/switchtesting/switchlog/port_1.pinglog.txt &
+
+echo "The switch test has started. Please wait for the pingtest to compelte before interacting further."
+echo "Sleeping for the duration of the test"
+sleep 30
+echo "Test is likely finished. Please execute the results.sh script to review the pingtest."
+
