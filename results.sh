8c41f71d2483eb2923a485a2dcc485d587ba76bd
Cody McDonald <codylmcdonald@gmail.com>
2023-01-15 06:35:01 -0800

    I modified filename that logs are written to. I also added the cp command to create a timestamped version of the file in the completedtests directory

--- results.sh
+++ results.sh
@@ -1,86 +1,90 @@
+ #results.sh
 #!/bin/bash
 #appends results for each port test to one txt file for easy parsing
- 
-echo "Port 1:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 2:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 3:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 4:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 5:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 6:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 7:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 8:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 9:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 10:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 11:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 12:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 13:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 14:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 15:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 16:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 17:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 18:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 19:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 20:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 21:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 22:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 23:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 24:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 25:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 26:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
-echo "Port 27:" >> testlog_$(date +%Y%m%d).txt
-cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | tail --lines=2 | grep "packet loss" >> testlog_$(date +%Y%m%d).txt
-echo " " >> testlog_$(date +%Y%m%d).txt
+ /home/ansible/switchtesting/switchlog//home/ansible/switchtesting/switchlog/results.txt
+echo "Port 1:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 2:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 3:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 4:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 5:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 6:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 7:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 8:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 9:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 10:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 11:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 12:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 13:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 14:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 15:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 16:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 17:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 18:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 19:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 20:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 21:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 22:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 23:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 24:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 25:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 26:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
+echo "Port 27:" >> /home/ansible/switchtesting/switchlog/results.txt
+cat /home/ansible/switchtesting/switchlog/port_1.pinglog.txt | grep "packet loss" >> /home/ansible/switchtesting/switchlog/results.txt
+echo " " >> /home/ansible/switchtesting/switchlog/results.txt
 
-cat testlog_$(date +%Y%m%d).txt
+
+
+cat /home/ansible/switchtesting/switchlog/results.txt
+cp /home/ansible/switchtesting/switchlog/results.txt /home/ansible/switchtesting/completedtests/results_$(date +%Y%m%d_%H%M%S).txt
