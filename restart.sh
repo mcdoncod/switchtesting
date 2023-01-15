04311a65ed139596e130e9992a6351f0ed5e00e7
Cody McDonald <codylmcdonald@gmail.com>
2023-01-15 07:45:33 -0800

    Added a prompt to run restart.sh via an if else script. Couldn't do this with rm -i as it prompted for each file.

--- restart.sh
+++ restart.sh
@@ -1,10 +1,17 @@
 #restart.sh
 #!/bin/bash
 echo "Are you sure you want to restart the switchtest?"
+read -p "(This will delete the files in the switchlog) (yes/no) " answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    echo "Proceeding..."
+    sleep 2	
+    rm /home/switchtesting/switchlog/*.txt
+    echo "You have just deleted the files in the /home/switchtesting/switchlog/ directory."
+    echo "The test has been reset. Please hook up a new switch and run the test again."
+    echo "/home/switchtesting/runtest.sh"	
+else
+    echo "Exiting..."
+    exit
+fi
 
-rm -i /home/switchtesting/switchlog/*.txt
-
-echo "You have just removed the files in the /home/switchtesting/switchlog/ directory."
-echo "The test has been reset. Please hook up a new switch and run the test again."
-echo "/home/switchtesting/runtest.sh"
 
