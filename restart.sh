28b4aa0ebafcce87fcc9f4845a52ffec30b763d7
Cody McDonald <codylmcdonald@gmail.com>
2023-01-15 06:30:53 -0800

    Modified restart so that it's only removing the contents of the switchlog directory.

--- restart.sh
+++ restart.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-mv /home/ansible/switchtesting/*.txt /home/ansible/switchtesting/completedtests
-rm /home/ansible/switchtesting/switchlog/*
+rm /home/ansible/switchtesting/switchlog/*.txt
